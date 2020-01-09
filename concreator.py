d4bc7cd72537b8a871e9ab350f23c0c43534814e
Efthymios Grivas <58295422+thimiosgr@users.noreply.github.com>
2020-01-09 18:45:02 +0200

    Create concreator.py

---
+++ concreator.py
@@ -0,0 +1,17 @@
+import commands
+import os
+import subprocess
+
+print '\nCreating container\n'
+
+out = commands.getoutput('docker run -d ubuntu /bin/bash -c "while true; do sleep 3600; done"')
+conname  = out[0:12]
+
+print 'Updating...\n'
+os.system('docker exec -it %s apt update' %conname)
+
+print 'Installing Ifconfig...'
+os.system('docker exec -it %s apt-get install net-tools' %conname)
+
+print 'Installing ping...'
+os.system('docker exec -it %s apt-get install iputils-ping -y' %conname)
