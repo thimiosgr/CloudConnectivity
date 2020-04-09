5bb59b6993b9e62a33e0c205d7e2f36885c66cca
thimiosgr <thimiosgr17@gmail.com>
2020-04-09 14:47:21 +0300

    Test commit

--- CloudConnectivity/cloudconnectivity.sh
+++ CloudConnectivity/cloudconnectivity.sh
@@ -6,12 +6,8 @@ sudo chmod 666 imagebuild.json connectivity.service
 PARAMS=""
 while (( "$#" )); do
   case "$1" in
-    -opip1)
-      OPENSTACK_IP1=$2
-      shift 2
-      ;;
-    -opip2)
-      OPENSTACK_IP2=$2
+    -opip)
+      OPENSTACK_IP=$2
       shift 2
       ;;
     -vpnip)
@@ -24,10 +20,6 @@ while (( "$#" )); do
       ;;
     -network)
       NETWORK_NAME=$2
-      shift 2
-      ;;
-    -password)
-      PASSWD=$2
       shift 2
       ;;
     -help)
@@ -58,8 +50,6 @@ if ! [[ -z $COUNTER ]]; then
   exit 1
 fi
 
-source admin-openrc1.sh $PASSWD
-
 # Checking if command-line JSON processor jq is installed.
 if ! dpkg -s jq >/dev/null 2>&1; then
     echo "Installing jq."
@@ -81,13 +71,8 @@ fi
 # Regular expression to check if IP's given by user are correct.
 IP_RE="^(([0-9]|[0-9]{2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[0-9]{2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])+$"
 
-if ! [[ ${OPENSTACK_IP1} =~ ${IP_RE} ]]; then
-  printf "\033[0;31mThe first Openstack IP is not correct\033[0m\n"
-  exit 1
-fi
-
-if ! [[ ${OPENSTACK_IP2} =~ ${IP_RE} ]]; then
-  printf "\033[0;31mThe second Openstack IP is not correct\033[0m\n"
+if ! [[ ${OPENSTACK_IP} =~ ${IP_RE} ]]; then
+  printf "\033[0;31mThe Openstack IP is not correct\033[0m\n"
   exit 1
 fi
 
@@ -109,8 +94,8 @@ if [[ -z "${NETWORK_ID}" ]]; then
   exit 1
 fi
 
-IDENTITY1="http://${OPENSTACK_IP1}/identity"
-jq --arg v "${IDENTITY1}" '.builders[].identity_endpoint = $v' imagebuild.json|sponge imagebuild.json
+IDENTITY="http://${OPENSTACK_IP}/identity"
+jq --arg v "${IDENTITY}" '.builders[].identity_endpoint = $v' imagebuild.json|sponge imagebuild.json
 jq --arg v "${IMAGE_ID}" '.builders[].source_image = $v' imagebuild.json|sponge imagebuild.json
 jq --arg v "${NETWORK_ID}" '.builders[].networks[] = $v' imagebuild.json|sponge imagebuild.json
 
@@ -125,28 +110,6 @@ sed -i '5s/.*/VPN_IP='"$VPN_IP"'/' bootscript.sh
 
 echo "Building image... This might take some minutes, depending on your hardware and your Internet connection."
 packer build imagebuild.json > /dev/null 2>&1
-printf "\033[0;32mCreated image: packerimage.\n\033[0mRun 'openstack image list' for confirmation.\n"
-
-printf "\nCreating server for testing...\n"
-openstack server create --image packerimage --flavor m1.heat_int --key-name KEYPAIR --user-data ${THE_PATH}/user-data.txt --network ${NETWORK_ID} testingserver
-printf "\n\033[0;32mCreated server 'testingserver'.\033[0m\nRun 'openstack server list' for confirmation.\n"
-
-source admin-openrc2.sh $PASSWD
-
-IDENTITY2="http://${OPENSTACK_IP2}/identity"
-jq --arg v "${IDENTITY2}" '.builders[].identity_endpoint = $v' imagebuild2.json|sponge imagebuild2.json
-jq --arg v "${IMAGE_ID}" '.builders[].source_image = $v' imagebuild2.json|sponge imagebuild2.json
-jq --arg v "${NETWORK_ID}" '.builders[].networks[] = $v' imagebuild2.json|sponge imagebuild2.json
-
-THE_PATH=$('pwd')
-BOOT_SCRIPT="${THE_PATH}/bootscript.sh"
-jq --arg v "$BOOT_SCRIPT" '.provisioners[0].source = $v' imagebuild2.json|sponge imagebuild2.json
-
-SERVICE="${THE_PATH}/connectivity.service"
-jq --arg v "$SERVICE" '.provisioners[1].source = $v' imagebuild2.json|sponge imagebuild2.json
-
-echo "Building image... This might take some minutes, depending on your hardware and your Internet connection."
-packer build imagebuild2.json > /dev/null 2>&1
 printf "\033[0;32mCreated image: packerimage.\n\033[0mRun 'openstack image list' for confirmation.\n"
 
 printf "\nCreating server for testing...\n"
