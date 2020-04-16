ea18ec7b130bf3d63e15f5c8e4e2cc6d39cd8439
webserver <webserver@webserver.168.1.18>
2020-04-16 12:21:52 +0300

    Blank

--- CloudConnectivity/openstack1.sh
+++ CloudConnectivity/openstack1.sh
@@ -1,6 +1,6 @@
-export OP_IP=150.140.186.115
-export VPN_IP=91.140.33.10
-export FILE=client1
-export PASSWORD=ops
-export NETWORK=net1
-export IMAGE=xenial1
+export OP_IP="150.140.186.115"
+export VPN_IP="91.140.33.10"
+export FILE="client1"
+export PASSWORD="ops"
+export NETWORK="net1"
+export IMAGE="xenial1"
