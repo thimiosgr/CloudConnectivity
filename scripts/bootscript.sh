7aef0dd5ed0688e66695233fa85a5157a2164373
thimiosgr <thimiosgr17@gmail.com>
2020-05-10 16:27:11 +0300

    blank

--- scripts/bootscript.sh
+++ scripts/bootscript.sh
@@ -11,18 +11,20 @@ FILE=/home/ubuntu/temp
 
 if [[ -f "$FILE" ]]; then
     if ! [ -f "${CHECK_FILE}" ]; then
-        echo "Fetching the VPN files..."
         wget http://${VPN_IP}/${USERNAME}/ca.crt -P /home/ubuntu/${USERNAME}/
         wget http://${VPN_IP}/${USERNAME}/${USERNAME}.crt -P /home/ubuntu/${USERNAME}/
         wget http://${VPN_IP}/${USERNAME}/${USERNAME}.key -P /home/ubuntu/${USERNAME}/
         wget http://${VPN_IP}/${USERNAME}/${USERNAME}.ovpn -P /home/ubuntu/${USERNAME}/
-        wget http://${VPN_IP}/${USERNAME}/where.txt -P /home/ubuntu/${USERNAME}/
-        # PEER_IP=$(head -n1 /home/ubuntu/${USERNAME}/where.txt)
-        # TUNNEL_IP=$(head -n2 /home/ubuntu/${USERNAME}/where.txt | tail -1)
+        wget http://${VPN_IP}/${USERNAME}/vtep.txt -P /home/ubuntu/${USERNAME}/
+        VTEP_IP=$(head -n1 /home/ubuntu/${USERNAME}/vtep.txt)
         sleep 3
+        IN_NET_IP="$(ip a | awk '/ens4/{getline;getline; print}' | awk -F/ '{print $1}' - | awk '{print $2}' -)/24"
         # OpenvSwitch configuration
+        sudo ovs-vsctl add-br br0
+        sudo ovs-vsctl add-port br0 vxlan0 -- set interface vxlan0 type=vxlan options:remote_ip=${VTEP_IP}
+        sudo ip addr flush ens4
+        sudo ifconfig br0 ${IN_NET_IP} up
+        sudo openvpn /home/ubuntu/${USERNAME}/${USERNAME}.ovpn
     fi
 fi
 
-echo "Establishing VPN connection..."
-sudo openvpn /home/ubuntu/${USERNAME}/${USERNAME}.ovpn
--- scripts/cloudconnectivity.sh
+++ scripts/cloudconnectivity.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Give permissions to the JSON and Service file.
-chmod 666 imagebuild.json connectivity.service
-
+chmod 666 imagebuild.json
 PARAMS=""
 while (( "$#" )); do
   case "$1" in
@@ -18,8 +17,12 @@ while (( "$#" )); do
       IMAGE_NAME=$2
       shift 2
       ;;
-    -n | -network)
-      NETWORK_NAME=$2
+    -xn | -xnet)
+      EXTERNAL_NETWORK=$2
+      shift 2
+      ;;
+    -in | -inet)
+      INTERNAL_NETWORK=$2
       shift 2
       ;;
     -p | -password)
@@ -46,7 +49,7 @@ while (( "$#" )); do
     -*|--*=) # unsupported flags
       echo "Error: Unsupported flag $1" >&2
       COUNTER=1
-      break
+      exit 1
       ;;
     *) # preserve positional arguments
       PARAMS="${PARAMS} $1"
@@ -62,7 +65,8 @@ if ! [[ -z ${COUNTER} ]]; then
   printf "\t\033[0;33m-oi\033[0m, \033[0;33m-opip\033[0m      :  The IP of the machine that runs the Openstack cloud.\n"
   printf "\t\033[0;33m-vi\033[0m, \033[0;33m-vpnip\033[0m     :  The IP of the machine that runs the OpenVPN server.\n"
   printf "\t\033[0;33m-i\033[0m, \033[0;33m-image\033[0m      :  The name of the image that will be used as source for the new image.\n"
-  printf "\t\033[0;33m-n\033[0m, \033[0;33m-network\033[0m    :  The name of the network on which the test server will be connected to.\n"
+  printf "\t\033[0;33m-xn\033[0m, \033[0;33m-xnet\033[0m      :  The name of the external network on which the OVS machine will be connected to.\n"
+  printf "\t\033[0;33m-in\033[0m, \033[0;33m-inet\033[0m      :  The name of the internal network on which the OVS machine and the rest instances will be connected to.\n"
   printf "\t\033[0;33m-p\033[0m, \033[0;33m-password\033[0m   :  The password of the Openstack cloud.\n"
   printf "\t\033[0;33m-f\033[0m, \033[0;33m-filename\033[0m   :  The name of the VPN files that every instance must fetch.\n"
   printf "\t\033[0;33m-def\033[0m, \033[0;33m-default\033[0m  :  Run script with the default values.\n"
@@ -81,17 +85,22 @@ if [[ -z "$VPN_IP" ]]; then
 fi
 
 if [[ -z "$IMAGE_NAME" ]]; then
-  printf "\033[0;31mYou have not provided an IMAGE name. Use -i or -image to provide one.\033[0m\n"
+  printf "\033[0;31mYou have not provided an image name. Use -i or -image to provide one.\033[0m\n"
   exit 1
 fi
 
-if [[ -z "$NETWORK_NAME" ]]; then
-  printf "\033[0;31mYou have not provided a NETWORK name. Use -i or -image to provide one.\033[0m\n"
+if [[ -z "$EXTERNAL_NETWORK" ]]; then
+  printf "\033[0;31mYou have not provided a name for the external network. Use -ei or -xnet to provide one.\033[0m\n"
+  exit 1
+fi
+
+if [[ -z "$INTERNAL_NETWORK" ]]; then
+  printf "\033[0;31mYou have not provided a name for the internal network. Use -in or -inet to provide one.\033[0m\n"
   exit 1
 fi
 
 if [[ -z "$PASSWD" ]]; then
-  printf "\033[0;31mYou have not provided a PASSWORD for authentication. Use -p or -password to provide one.\033[0m\n"
+  printf "\033[0;31mYou have not provided a password for authentication. Use -p or -password to provide one.\033[0m\n"
   exit 1
 fi
 
@@ -149,7 +158,8 @@ fi
 
 # Converting image and network names to ID's, so they can be passed to Packer JSON file.
 IMAGE_ID=$(openstack image list | grep ${IMAGE_NAME} | awk '{print $2}' -)
-NETWORK_ID=$(openstack network list | grep ${NETWORK_NAME} | awk '{print $2}' -)
+EXTERNAL_NETWORK_ID=$(openstack network list | grep ${EXTERNAL_NETWORK} | awk '{print $2}' -)
+INTERNAL_NETWORK_ID=$(openstack network list | grep ${INTERNAL_NETWORK} | awk '{print $2}' -)
 
 # Checking if the image and network given by user are correct.
 if [[ -z "${IMAGE_ID}" ]]; then
@@ -157,8 +167,13 @@ if [[ -z "${IMAGE_ID}" ]]; then
   exit 1
 fi
 
-if [[ -z "${NETWORK_ID}" ]]; then
-  printf "\033[0;31mThe network name you provided is not correct.\033[0m\n"
+if [[ -z "${EXTERNAL_NETWORK_ID}" ]]; then
+  printf "\033[0;31mThe external network name you provided is not correct.\033[0m\n"
+  exit 1
+fi
+
+if [[ -z "${INTERNAL_NETWORK_ID}" ]]; then
+  printf "\033[0;31mThe internal network name you provided is not correct.\033[0m\n"
   exit 1
 fi
 
@@ -166,14 +181,14 @@ fi
 IDENTITY="http://${OPENSTACK_IP}/identity"
 jq --arg v "${IDENTITY}" '.builders[].identity_endpoint = $v' imagebuild.json|sponge imagebuild.json
 jq --arg v "${IMAGE_ID}" '.builders[].source_image = $v' imagebuild.json|sponge imagebuild.json
-jq --arg v "${NETWORK_ID}" '.builders[].networks[] = $v' imagebuild.json|sponge imagebuild.json
+jq --arg v "${EXTERNAL_NETWORK_ID}" '.builders[].networks[] = $v' imagebuild.json|sponge imagebuild.json
 
 THE_PATH=$('pwd')
-BOOT_SCRIPT="${THE_PATH}/bootscript.sh"
-jq --arg v "${BOOT_SCRIPT}" '.provisioners[0].source = $v' imagebuild.json|sponge imagebuild.json
+TUNNEL_SCRIPT="${THE_PATH}/bootscript.sh"
+jq --arg v "${TUNNEL_SCRIPT}" '.provisioners[0].source = $v' imagebuild.json|sponge imagebuild.json
 
-SERVICE="${THE_PATH}/connectivity.service"
-jq --arg v "${SERVICE}" '.provisioners[1].source = $v' imagebuild.json|sponge imagebuild.json
+TUNNEL_SERVICE="${THE_PATH}/connectivity.service"
+jq --arg v "${TUNNEL_SERVICE}" '.provisioners[1].source = $v' imagebuild.json|sponge imagebuild.json
 
 NETWORKING_SCRIPT="${THE_PATH}/networkconfiguration.sh"
 jq --arg v "${NETWORKING_SCRIPT}" '.provisioners[2].source = $v' imagebuild.json|sponge imagebuild.json
@@ -190,7 +205,7 @@ packer build imagebuild.json > /dev/null 2>&1
 printf "\033[0;32mCreated image: packerimage.\n\033[0mRun 'openstack image list' for confirmation.\n"
 
 printf "\nCreating server for testing...\n"
-openstack server create --image packerimage --flavor m1.heat_int --key-name KEYPAIR --user-data ${THE_PATH}/user-data.txt --network ${NETWORK_ID} --network internal ovsmachine
+openstack server create --image packerimage --flavor m1.heat_int --key-name KEYPAIR --user-data ${THE_PATH}/user-data.txt --network $EXTERNAL_NETWORK_ID --network $INTERNAL_NETWORK_ID ovsmachine
 printf "\n\033[0;32mCreated server 'testingserver'.\033[0m\nRun 'openstack server list' for confirmation.\n"
 
 sleep 10
