678b71cc8c01b2533abd837643cbecdae5ca6b6e
thimiosgr <thimiosgr17@gmail.com>
2020-05-10 13:14:29 +0300

    new

--- scripts/bootscript.sh
+++ scripts/bootscript.sh
@@ -8,23 +8,25 @@ USERNAME="client1"
 sudo mkdir /home/ubuntu/${USERNAME}
 CHECK_FILE=/home/ubuntu/${USERNAME}/ca.crt
 
-if ! [ -f "${CHECK_FILE}" ]; then
-    echo "Fetching the VPN files..."
-    wget http://${VPN_IP}/${USERNAME}/ca.crt -P /home/ubuntu/${USERNAME}/
-    wget http://${VPN_IP}/${USERNAME}/${USERNAME}.crt -P /home/ubuntu/${USERNAME}/
-    wget http://${VPN_IP}/${USERNAME}/${USERNAME}.key -P /home/ubuntu/${USERNAME}/
-    wget http://${VPN_IP}/${USERNAME}/${USERNAME}.ovpn -P /home/ubuntu/${USERNAME}/
-    wget http://${VPN_IP}/${USERNAME}/where.txt -P /home/ubuntu/${USERNAME}/
-    PEER_IP=$(head -n1 /home/ubuntu/${USERNAME}/where.txt)
-    TUNNEL_IP=$(head -n2 /home/ubuntu/${USERNAME}/where.txt | tail -1)
-    sleep 3
-    # OpenvSwitch configuration
-    sudo ip link add veth0 type veth peer name veth1
-    sudo ifconfig veth0 up
-    sudo ifconfig veth1 ${TUNNEL_IP}/24 up
-    sudo ovs-vsctl add-br br0
-    sudo ovs-vsctl add-port br0 vxlan0 -- set interface vxlan0 type=vxlan options:remote_ip=${PEER_IP}
-    sudo ovs-vsctl add-port br0 veth0
+if [[ -f "$FILE" ]]; then
+    if ! [ -f "${CHECK_FILE}" ]; then
+        echo "Fetching the VPN files..."
+        wget http://${VPN_IP}/${USERNAME}/ca.crt -P /home/ubuntu/${USERNAME}/
+        wget http://${VPN_IP}/${USERNAME}/${USERNAME}.crt -P /home/ubuntu/${USERNAME}/
+        wget http://${VPN_IP}/${USERNAME}/${USERNAME}.key -P /home/ubuntu/${USERNAME}/
+        wget http://${VPN_IP}/${USERNAME}/${USERNAME}.ovpn -P /home/ubuntu/${USERNAME}/
+        wget http://${VPN_IP}/${USERNAME}/where.txt -P /home/ubuntu/${USERNAME}/
+        PEER_IP=$(head -n1 /home/ubuntu/${USERNAME}/where.txt)
+        TUNNEL_IP=$(head -n2 /home/ubuntu/${USERNAME}/where.txt | tail -1)
+        sleep 3
+        # OpenvSwitch configuration
+        sudo ip link add veth0 type veth peer name veth1
+        sudo ifconfig veth0 up
+        sudo ifconfig veth1 ${TUNNEL_IP}/24 up
+        sudo ovs-vsctl add-br br0
+        sudo ovs-vsctl add-port br0 vxlan0 -- set interface vxlan0 type=vxlan options:remote_ip=${PEER_IP}
+        sudo ovs-vsctl add-port br0 veth0
+    fi
 fi
 
 echo "Establishing VPN connection..."
