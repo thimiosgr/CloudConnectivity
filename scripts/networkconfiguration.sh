b255bad2fa8728e5b410558f71fbd56d1242b732
thimiosgr <thimiosgr17@gmail.com>
2020-05-11 12:15:54 +0300

    new

--- scripts/networkconfiguration.sh
+++ scripts/networkconfiguration.sh
@@ -6,5 +6,6 @@ if ! [[ -f "$FILE" ]]; then
     sudo bash -c "echo auto ens4 >> /etc/network/interfaces.d/50-cloud-init.cfg"
     sudo bash -c "echo iface ens4 inet dhcp >> /etc/network/interfaces.d/50-cloud-init.cfg"
     touch /home/ubuntu/temp
+    sleep 5
     reboot
 fi
