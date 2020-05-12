c4c21766121676dbb53a069dcd18b4c612e6f259
thimiosgr <thimiosgr17@gmail.com>
2020-05-12 15:55:13 +0300

    new

--- services/tunnelcreator.sh
+++ services/tunnelcreator.sh
@@ -20,13 +20,13 @@ if [[ -f "$FILE" ]]; then
         sleep 3
         IN_NET_IP="$(ip a | awk '/ens4/{getline;getline; print}' | awk -F/ '{print $1}' - | awk '{print $2}' -)/24"
         # OpenvSwitch configuration
-        sudo ip route add default via 192.168.1.1
         sudo ovs-vsctl add-br br0
         sudo ip addr flush dev ens4
         sudo ovs-vsctl add-port br0 ens4
         sudo ip addr add ${IN_NET_IP} dev br0
         sudo ip link set br0 up
         sudo ovs-vsctl add-port br0 vxlan0 -- set interface vxlan0 type=vxlan options:remote_ip=${VTEP_IP}
+        sudo ip route add default via 192.168.1.1
         sudo openvpn /home/ubuntu/${USERNAME}/${USERNAME}.ovpn
     fi
 fi
