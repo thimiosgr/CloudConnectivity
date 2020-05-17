95b3e25e31592a01b7adbbaf8ae1c3136f167647
thimiosgr <thimiosgr17@gmail.com>
2020-05-17 11:51:07 +0300

    new

--- services/tunnelcreator.sh
+++ services/tunnelcreator.sh
@@ -26,56 +26,61 @@ if [[ -f "$FILE" ]]; then
         IN_NET_IP3="$(ip a | awk '/ens6/{getline;getline; print}' | awk -F/ '{print $1}' - | awk '{print $2}' -)/24"
         IN_NET_IP4="$(ip a | awk '/ens7/{getline;getline; print}' | awk -F/ '{print $1}' - | awk '{print $2}' -)/24"
 
-        ip link add veth0 type veth peer name veth1
-        ip link add veth2 type veth peer name veth3
-        ip link add veth4 type veth peer name veth5
-        ip link add veth6 type veth peer name veth7
+        IP1_MD5=$(md5sum <<<"${IN_NET_IP1}" | cut -c 1-10)
+        IP2_MD5=$(md5sum <<<"${IN_NET_IP2}" | cut -c 1-10)
+        IP3_MD5=$(md5sum <<<"${IN_NET_IP3}" | cut -c 1-10)
+        IP4_MD5=$(md5sum <<<"${IN_NET_IP4}" | cut -c 1-10)
+
+        ip link add $IP1_MD5 type veth peer name "V${IP1_MD5}"
+        ip link add $IP2_MD5 type veth peer name "V${IP2_MD5}"
+        ip link add $IP3_MD5 type veth peer name "V${IP3_MD5}"
+        ip link add $IP4_MD5 type veth peer name "V${IP4_MD5}"
         
         # OpenvSwitch configuration
-        sudo ovs-vsctl add-br br0
+        sudo ovs-vsctl add-br "br${IP1_MD5}"
         ip addr flush dev ens4
-        sudo ovs-vsctl add-port br0 ens4
-        sudo ovs-vsctl add-port br0 veth0
-        ifconfig veth0 up
-        ip addr add ${IN_NET_IP1} dev br0
-        ip link set br0 up
+        sudo ovs-vsctl add-port "br${IP1_MD5}" ens4
+        sudo ovs-vsctl add-port "br${IP1_MD5}" $IP1_MD5
+        ifconfig $IP1_MD5 up
+        ip addr add ${IN_NET_IP1} dev "br${IP1_MD5}"
+        ip link set "br${IP1_MD5}" up
 
-        sudo ovs-vsctl add-br br1
+        sudo ovs-vsctl add-br "br${IP2_MD5}"
         ip addr flush dev ens5
-        sudo ovs-vsctl add-port br1 ens5
-        sudo ovs-vsctl add-port br1 veth2
-        ifconfig veth2 up
-        ip addr add ${IN_NET_IP2} dev br1
-        ip link set br1 up
+        sudo ovs-vsctl add-port "br${IP2_MD5}" ens5
+        sudo ovs-vsctl add-port "br${IP2_MD5}" $IP2_MD5
+        ifconfig $IP2_MD5 up
+        ip addr add ${IN_NET_IP2} dev "br${IP2_MD5}"
+        ip link set "br${IP2_MD5}" up
 
-        sudo ovs-vsctl add-br br2
+        sudo ovs-vsctl add-br "br${IP3_MD5}"
         ip addr flush dev ens6
-        sudo ovs-vsctl add-port br2 ens6
-        sudo ovs-vsctl add-port br2 veth4
-        ifconfig veth4 up
-        ip addr add ${IN_NET_IP3} dev br2
-        ip link set br2 up
+        sudo ovs-vsctl add-port "br${IP3_MD5}" ens6
+        sudo ovs-vsctl add-port "br${IP3_MD5}" $IP3_MD5
+        ifconfig $IP3_MD5 up
+        ip addr add ${IN_NET_IP3} dev "br${IP3_MD5}"
+        ip link set "br${IP3_MD5}" up
 
-        sudo ovs-vsctl add-br br3
+        sudo ovs-vsctl add-br "br${IP4_MD5}"
         ip addr flush dev ens7
-        sudo ovs-vsctl add-port br3 ens7
-        sudo ovs-vsctl add-port br3 veth6
-        ifconfig veth6 up
-        ip addr add ${IN_NET_IP4} dev br3
-        ip link set br3 up
+        sudo ovs-vsctl add-port "br${IP4_MD5}" ens7
+        sudo ovs-vsctl add-port "br${IP4_MD5}" $IP4_MD5
+        ifconfig $IP4_MD5 up
+        ip addr add ${IN_NET_IP4} dev "br${IP4_MD5}"
+        ip link set "br${IP4_MD5}" up
 
         sudo ovs-vsctl add-br tunnelbr
-        sudo ovs-vsctl add-port tunnelbr veth1
-        ifconfig veth1 up
-        sudo ovs-vsctl add-port tunnelbr veth3
-        ifconfig veth3 up
-        sudo ovs-vsctl add-port tunnelbr veth5
-        ifconfig veth5 up
-        sudo ovs-vsctl add-port tunnelbr veth7
-        ifconfig veth7 up
+        sudo ovs-vsctl add-port tunnelbr "V${IP1_MD5}"
+        ifconfig "V${IP1_MD5}" up
+        sudo ovs-vsctl add-port tunnelbr "V${IP2_MD5}"
+        ifconfig "V${IP1_MD5}" up
+        sudo ovs-vsctl add-port tunnelbr "V${IP3_MD5}"
+        ifconfig "V${IP3_MD5}" up
+        sudo ovs-vsctl add-port tunnelbr "V${IP4_MD5}"
+        ifconfig "V${IP4_MD5}" up
         sudo ovs-vsctl add-port tunnelbr vxlan0 -- set interface vxlan0 type=vxlan options:remote_ip=${VTEP_IP}
         openvpn /home/ubuntu/${USERNAME}/${USERNAME}.ovpn 
-        
+
     fi
 fi
 
